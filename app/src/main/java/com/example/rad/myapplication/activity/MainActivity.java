c5d871935ac796fbd019edb8c3dafd05b45a91d7
Radek <radomich11@gmail.com>
2019-01-29 20:30:18 +0100

    first view

---
+++ app/src/main/java/com/example/rad/myapplication/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.rad.myapplication.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rad.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
